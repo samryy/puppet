8de46fc423a15471fd6578fe4523f03d66aae515
Sami Ryyppö <sryypp@gmail.com>
2017-04-11 13:25:52 +0300

    Second homework commit

---
+++ apachemod/manifests/init.pp
@@ -0,0 +1,18 @@
+class apache {
+        package {apache2:
+                ensure => 'installed', 
+                allowcdrom => 'true',
+        }
+  
+        file {'/var/www/html/index.html': 
+                 content => "Sami's testsite\n",
+                 require => Package["apache2"];
+        }
+
+         service {apache2:
+                ensure => 'running', 
+                enable => 'true', 
+                subscribe => File ['/var/www/html/index.html'],
+        }
+}
+
