3ae0e4e6aa440a95767412259f8df9b62460a763
Sami Ryyppö <sryypp@gmail.com>
2017-05-15 16:29:43 +0000

    Update start.sh

--- setupmod/start.sh
+++ setupmod/start.sh
@@ -2,8 +2,8 @@ setxkbmap fi
 sudo add-apt-repository universe
 sudo apt-get update
 sudo apt-get -y install puppet git
-git clone https://github.com/sryyppo/puppet
 cd /etc/puppet/
-sudo mv puppet modules
+git clone https://github.com/sryyppo/puppet
+sudo mv /home/puppet modules
 sudo puppet apply --modulepath puppet/modules/ -e 'class {"setupmod":}'
 
