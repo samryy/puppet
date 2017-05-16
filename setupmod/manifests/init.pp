3a7133dad224207f122b82adfc969a17bb469430
Sami Ryyppö <sryypp@gmail.com>
2017-05-16 10:13:10 +0300

    Update init.pp

--- setupmod/manifests/init.pp
+++ setupmod/manifests/init.pp
@@ -2,11 +2,8 @@ class setupmod {
 
 ## Programs to install
 
-	$user = 'sami'
+	$user = 'ubuntu'
 	$setup = [ 'mysql-server', 'mysql-client', 'npm', 'nodejs', 'php7.0', 'libapache2-mod-php', 'php-mysql' ]
-	$vhostPath = "/etc/apache2/sites-enabled"
-	$title = 'sami'
-
 
 ## Check for updates
 
@@ -51,26 +48,10 @@ class setupmod {
                 ensure => 'installed', 
                 allowcdrom => 'true',
         }
-	
-	define apachevhost {
-		file { "/etc/apache2/sites-enabled/${title}.conf":
-			ensure	=> 'present',
-			mode	=> '0644',
-			content	=> template("apache/vhost.conf.erb"),
-			require => Package['apache'],
-			notify  => Service['apache'],
-		}
 
-		file { "/var/www/${title}/":
-			ensure	=> 'directory',
-			mode	=> '0755',
-		}
-
-		file { "/var/www/${title}/index.html":
-			ensure	=> 'present',
-			mode	=> '0644',
-			content	=> "This is a virtualhost ${title}\n"
-		}
+	file { '/var/www/html/index.html':
+		ensure => 'present',
+		content => template('setupmod/index.html.erb')
 	}
 
 }
