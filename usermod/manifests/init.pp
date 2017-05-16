ba629b87450d76bde9e462ecaa5e9566ee0630e3
Sami Ryyppo <sryypp@gmail.com>
2017-05-16 09:30:16 +0300

    Changes

--- usermod/manifests/init.pp
+++ usermod/manifests/init.pp
@@ -1,6 +1,10 @@
 class usermod {
 
-$userlist = [ 'JaakkoK', 'JesseL', 'NiinaK' ]
+
+$setup = [ 'mysql-server', 'mysql-client', 'npm', 'nodejs', 'php7.0', 'libapache2-mod-php', 'php-mysql', ]
+
+$userlist = 'JaakkoK'
+$password = 'hello'
 
 	user { $userlist:
 		ensure	 => 'present',
@@ -11,6 +15,46 @@ $userlist = [ 'JaakkoK', 'JesseL', 'NiinaK' ]
 		password => generate('/bin/sh', '-c', "mkpasswd -m sha-512 ${password} | tr -d '\n'"),
 
 	}
+	file { "/home/${userlist}/public_html":
+		ensure => 'directory',
+		group  => '1000',
+		mode   => '775',
+		owner  => '1000',
+
+	}
+
+	file { "/home/${userlist}/programming":
+		ensure => 'directory',
+		group  => '1000',
+		mode   => '775',
+		owner  => '1000',
+
+	}
+
+	file { "/home/${userlist}/git":
+		ensure => 'directory',
+		group  => '1000',
+		mode   => '775',
+		owner  => '1000',
+
+	}
+
+	package { $setup:
+		ensure => 'installed', 
+                allowcdrom => 'true',
+
+	}
+
+	package {apache2:
+                ensure => 'installed', 
+                allowcdrom => 'true',
+        }
+
+	file { '/var/www/html/index.html':
+		ensure => 'present',
+		content => template("usermod/index.html.erb")
+	}
+
 
 	
 
